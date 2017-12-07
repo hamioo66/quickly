17869c5c9d560548b50caf1bc10d5e1aae816e6d
hamioo <zhm200701@126.com>
2017-12-07 14:55:16 +0800

    集鲜丰添加代理商

--- testsuits/添加代理商.py
+++ testsuits/添加代理商.py
@@ -2,23 +2,22 @@
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.action_chains import ActionChains
-import time,os
+import time,os,unittest
 from selenium.webdriver.common.keys import Keys
 from utils.logger import Logger
 logger = Logger(logger="BasePage").getlog()
-
-user_file = open('../file/login.txt', 'r')
-values = user_file.readlines()
-user_file.close()
-i=[]
-for search in values:
-    i.append(search)
-driver=webdriver.Chrome()
-url='http://123.206.57.62:17000/index'
+#参数0、1、2分别对应管理员、批发商代理商
+driver = webdriver.Chrome()
+url = 'http://123.206.57.62:17000/index'
 driver.get(url)
 driver.maximize_window()
-#参数0、1、2分别对应管理员、批发商代理商
 def login(num):
+    user_file = open('../file/login.txt', 'r')
+    values = user_file.readlines()
+    user_file.close()
+    i = []
+    for search in values:
+        i.append(search)
     type=i[num].split(':')[0]
     keys=i[num].split(':')[1]
     username = keys.split(',')[0]
@@ -49,60 +48,62 @@ mainIframe=driver.find_elements_by_class_name('iframeBox')
 driver.switch_to.frame(mainIframe[-1])
 time.sleep(1)
 driver.find_element_by_css_selector('button.columnBtn.js_add_agent').click()
-
 mainIframe1=driver.find_elements_by_tag_name('iframe')
-print mainIframe1
 driver.switch_to.frame(mainIframe1[-1])
-driver.find_element_by_css_selector('input[placeholder="请输入企业名称"]').send_keys('123456')
-driver.find_element_by_css_selector('input[placeholder="有效的手机号码"]').send_keys('15599155289')
 
-driver.find_element_by_id('file').send_keys('C:\\Users\\asus1\\Pictures\\aa.jpg')
-time.sleep(5)
-# driver.find_element_by_xpath('//*[@id="agent_area_div"]/div[1]').click() #点击选择代理区域
-driver.find_element_by_id('file2').send_keys('C:\\Users\\asus1\\Pictures\\aa.jpg')
-# driver.find_element_by_css_selector('input#file2.fileImg').click()
-# #调用upfile.exe上传程序
-# os.system("F:\\upfile.exe")
-driver.find_element_by_id('file2').send_keys('C:\\Users\\asus1\\Pictures\\bb.jpg')
+def sendValueToInput(inputValues=[]):
+    allInputs = driver.find_elements_by_css_selector('li>input')
+    try:
+        i=0
+        while i<len(inputValues):
+                allInputs[i].send_keys(inputValues[i])
+                i=i+1
+    except:
+         print("参数值为空")
 
-
-
-
-def selectNum(num):
-    selects = driver.find_elements_by_css_selector('button[title="请选择"]')
-    print len(selects)
-    selects[num - 1].click()
-    if num==1:
-        selectArea()
-    elif num==3:
-        selectContactAddress()
-
-
-def selectArea(area=[]):     #代理区域
+def selectArea(area=[]):     #级联选择
     formSearchs = driver.find_elements_by_class_name('form-control')
     selects = driver.find_elements_by_css_selector('button[title="请选择"]')
-    print len(selects)
-    for i in area:
-        #print i
+    i = 0
+    while i < len(area):
         for j in range(0,len(area)):
-            if j<len(area):
-                #print j
+            try:
                 selects[j].click()
-                formSearchs[j].send_keys(i)
+                formSearchs[j].send_keys(area[i])
                 formSearchs[j].send_keys(Keys.ENTER)
+                i=i+1
+            except:
+                ValueError
+
+def FileImg(file):
+    imgFiles = driver.find_elements_by_css_selector('section>input')
+    print len(imgFiles)
+    try:
+        i=0
+        while i<len(imgFiles):
+            imgFiles[i].send_keys(file)
+            i=i+1
+    except:
+         print("参数值为空")
+
+def MoreImgFiles(file):  #上传更多图片，只针对第二张
+    try:
+        secondImgFile = driver.find_element_by_id("file2")
+        more=driver.find_element_by_id("file3")
+        secondImgFile.send_keys(file)
+        more.send_keys(file)
+    except:
+        print "当前元素不存在"
+if __name__=="__main__":
+    #测试添加代理商
+    area=[u'北京市',u'市辖区',u'北京市',u'市辖区',u'东城区',u'景山街道']
+    selectArea(area)
+    inputValues=[u'测试','15599155289','15599155289',u'测试地址地址']
+    sendValueToInput(inputValues)
+    FileImg('C:\\Users\\lenovo\\Pictures\\aa.jpg')
+    time.sleep(4)
+    MoreImgFiles('C:\\Users\\lenovo\\Pictures\\bb.jpg')
+    secondImgFile = driver.find_element_by_id("file3")
+    secondImgFile.send_keys('C:\\Users\\lenovo\\Pictures\\cc.jpg')
 
 
-
-def selectContactAddress(self,province,city,area,country):   #联系地址
-    # self.province = province
-    # self.city = city
-    # self.area=area
-    # self.country=country
-    # formSearchs = driver.find_elements_by_class_name('form-control')
-    # formSearchs[self.num - 1].send_keys(self.province)
-    # formSearchs[self.num - 1].send_keys(Keys.ENTER)
-    # formSearchs[self.num].send_keys(self.city)
-    # formSearchs[self.num - 1].send_keys(Keys.ENTER)
-    pass
-area=[u'北京市',u'市辖区']
-selectArea(area)
